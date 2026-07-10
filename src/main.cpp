6067cd99893d8801db0cf10a68cc68b5678a1aac
Oskar Choban <124447345+donotcallmehejlsberg@users.noreply.github.com>
2026-07-11 01:20:32 +0200

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,13 @@
 #include "Board/Board.hpp"
 #include "Board/BoardRenderer.hpp"
 #include "Game/GameSetup.hpp"
+#include "Game/Game.hpp"
 #include "Player.hpp"
 
 int main() {
-  Player white_player(Color::White);
-  Player black_player(Color::Black);
+  Game game;
+  game.run();
 
-  Board board;
-  GameSetup setup;
-  BoardRenderer renderer;
-
-  setup.setupPieces(board, white_player, black_player);
-  renderer.printBoard(board, white_player);
 
   return 0;
 }
